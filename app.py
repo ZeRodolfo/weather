7df30353b4ef5efc82a36da6c994ec884d4499bf
Jose Rodolfo P. Pessanha da Silva <jrodolfo@auxiliadora.censanet.com.br>
2019-03-08 15:39:49 -0300

    Criado um novo conversor de url para retornar um objeto do tipo data

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, Response, json
+from utils import DateConverter
 
 app = Flask(__name__)
+app.url_map.converters['date'] = DateConverter
 
-@app.route("/weather/local/<int:code>/from/<string:date_from>/to/<string:date_to>", methods=['GET'])
+@app.route("/weather/local/<int:code>/from/<date:date_from>/to/<date:date_to>", methods=['GET'])
 def weather_local(code, date_from, date_to):
     response_data = []
     return Response(json.dumps(response_data), status=200, mimetype='application/json')
---
+++ utils.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from werkzeug.routing import BaseConverter, ValidationError
+
+class DateConverter(BaseConverter):
+    regex = r'\d{4}-\d{2}-\d{2}'
+
+    def to_python(self, value):
+        try:
+            return datetime.strptime(value, '%Y-%m-%d').date()
+        except ValueError:
+            raise ValidationError()
+
+    def to_url(self, value):
+        return value.strftime('%Y-%m-%d')
